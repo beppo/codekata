021ed9f973e225e7caf1bfd98cdf1b84dcef12cd
Mehmet Ali Irmak <m.a.irmak@googlemail.com>
2015-10-24 00:12:34 +0100

    Solve anagrams using recursion

--- closure/src/codekata/anagrams/anagram.clj
+++ closure/src/codekata/anagrams/anagram.clj
@@ -3,7 +3,7 @@
   codekata.anagrams.anagram
   (require [clojure.string :as string]))
 
-(defn anagrams
+(defn is-anagram?
   "Recursively remove all letters from first word and second until there is a letter which is not in second word or all
   letters are removed from first word. Returns true if all letters are succesfully removed false otherwise"
   {:added "1.0"
@@ -12,15 +12,40 @@
   (if (= (count word1) (count word2))
     (if (= 0 (count word1))
       true
-      (anagrams (rest word1) (string/replace-first word2 (first word1) ""))
+      (recur (rest word1) (string/replace-first word2 (first word1) ""))
       )
     false
     )
   )
 
-;;(defn find-anagrams [dictionary]
-;;  "Finds all anagrams in the given dictionary"
-;;  )
+(defn is-words-anagrams?
+  [anagrams, word]
+  (if (empty? anagrams)
+    false
+    (is-anagram? word (first anagrams))
+    )
+  )
+
+(defn add-to-anagrams
+  [anagrams, word]
+  (if (empty? anagrams)
+    (cons (conj '() word) anagrams)
+    (if (is-words-anagrams? (first anagrams) word)
+      (cons  (conj (first anagrams) word) (rest anagrams))
+      (cons (first anagrams) (add-to-anagrams (rest anagrams) word) )
+      )
+    )
+  )
+
+(defn find-anagrams
+  "Finds all anagrams in the given dictionary"
+  [dictionary, anagrams]
+  (if (= 0 (count dictionary))
+    anagrams
+    (recur (rest dictionary) (add-to-anagrams anagrams (first dictionary)))
+    )
+ )
+
 
 ;; Possible algorithms to solve this problem
 ;; 1- While dictionary not empty
--- closure/test/codekata/anagrams/anagram_test.clj
+++ closure/test/codekata/anagrams/anagram_test.clj
@@ -2,12 +2,12 @@
   codekata.anagrams.anagram-test
   (require [clojure.test :refer :all] [codekata.anagrams.anagram :refer :all]))
 
-;;(deftest find-anagrams
-;;  (testing "0 maps to bit index 0" (is (= 0 (find-anagrams '("pictures" "piecrust" "sunders" "undress")))))
-;;  )
-
 (deftest anagrams-test
-  (testing "'a' and 'a' are anagrams" (is (= true (anagrams "a" "a"))))
-  (testing "'pictures' and 'piecrust' are anagrams" (is (= true (anagrams "pictures" "piecrust"))))
-  (testing "'dana' and 'mana' are not anagrams" (is (= false (anagrams "dana" "mana"))))
+  (testing "'a' and 'a' are anagrams" (is (= true (is-anagram? "a" "a"))))
+  (testing "'pictures' and 'piecrust' are anagrams" (is (= true (is-anagram? "pictures" "piecrust"))))
+  (testing "'dana' and 'mana' are not anagrams" (is (= false (is-anagram? "dana" "mana"))))
+  )
+
+(deftest find-anagrams-test
+  (testing "{elif, file, ben, sen, ali, lia} has anagrams {{file, elif}, {ben}, {sen}, {lia, ali}}" (is (= '(("file" "elif") ("ben") ("sen") ("lia" "ali")) (find-anagrams '("elif" "file" "ben" "sen" "ali" "lia") '()))))
   )
